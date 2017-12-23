60d7c2352910239e6444f95bb9de2ee873a5718d
Debal Chakrabarty <debalchakrabarty@gmail.com>
2017-12-23 08:59:55 +0530

    Update Makaut.py

--- practicalScripts/Makaut.py
+++ practicalScripts/Makaut.py
@@ -11,6 +11,7 @@ names=[]
 chars=['A','B','C','D','E','F','G']
 chars2=['H','I','J','K']
 subs=['Principles of management','Database management system','Computer Networking','Software engg','Computer Graphics','Discrete&Compiler Design']
+labs=['DBMS','CN','SE','Seminar']
 subject1=[]
 subject2=[]
 subject3=[]
@@ -34,15 +35,21 @@ for i in range(2,20):
     dbms.append(sheet[chars2[0]+str(i)].value)
     cn.append(sheet[chars2[1]+str(i)].value)
     se.append(sheet[chars2[2]+str(i)].value)
-    seminar.append(sheet[chars[3]+str(i)].value)
+    seminar.append(sheet[chars2[3]+str(i)].value)
 sub_mean=[np.array(subject1),np.array(subject2),np.array(subject3),np.array(subject4),np.array(subject5),np.array(subject6),np.array(dbms),np.array(cn),np.array(se),np.array(seminar)]
 print("Subjects\n")
+print(seminar)
 for i in range(0,6):
     plt.hist(sub_mean[i])
     plt.xlabel(subs[i])
+    plt.savefig("C:\\Users\\ratul\\Desktop\\img{number}.png".format(number=str(i)))
     plt.show()
 print("LAB\n")
+k=0
 for i in range(6,10):
     plt.hist(sub_mean[i])
+    plt.xlabel(labs[k])
+    plt.grid(True)
+    plt.savefig("C:\\Users\\ratul\\Desktop\\img{number}.png".format(number=str(i)))
     plt.show()
-
+    k+=1
