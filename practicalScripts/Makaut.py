6d6fb8f871f20ce55df30619ade2ec684306f87f
Debal Chakrabarty <debalchakrabarty@gmail.com>
2017-12-23 08:34:29 +0530

    Create Makaut.py

---
+++ practicalScripts/Makaut.py
@@ -0,0 +1,48 @@
+import os
+import openpyxl
+import numpy as np
+import matplotlib.pyplot as plt
+os.chdir('C:\\Users\\ratul\\Documents')
+
+workbook=openpyxl.load_workbook('exp.xlsx')
+sheet=workbook.get_sheet_by_name('Book1')
+
+names=[]
+chars=['A','B','C','D','E','F','G']
+chars2=['H','I','J','K']
+subs=['Principles of management','Database management system','Computer Networking','Software engg','Computer Graphics','Discrete&Compiler Design']
+subject1=[]
+subject2=[]
+subject3=[]
+subject4=[]
+subject5=[]
+subject6=[]
+dbms=[]
+cn=[]
+se=[]
+seminar=[]
+
+for i in range(2,20):
+    names.append(sheet[chars[0]+str(i)].value)
+    subject1.append(sheet[chars[1]+str(i)].value)
+    subject2.append(sheet[chars[2]+str(i)].value)
+    subject3.append(sheet[chars[3]+str(i)].value)
+    subject4.append(sheet[chars[4]+str(i)].value)
+    subject5.append(sheet[chars[5]+str(i)].value)
+    subject6.append(sheet[chars[6]+str(i)].value)
+for i in range(2,20):
+    dbms.append(sheet[chars2[0]+str(i)].value)
+    cn.append(sheet[chars2[1]+str(i)].value)
+    se.append(sheet[chars2[2]+str(i)].value)
+    seminar.append(sheet[chars[3]+str(i)].value)
+sub_mean=[np.array(subject1),np.array(subject2),np.array(subject3),np.array(subject4),np.array(subject5),np.array(subject6),np.array(dbms),np.array(cn),np.array(se),np.array(seminar)]
+print("Subjects\n")
+for i in range(0,6):
+    plt.hist(sub_mean[i])
+    plt.xlabel(subs[i])
+    plt.show()
+print("LAB\n")
+for i in range(6,10):
+    plt.hist(sub_mean[i])
+    plt.show()
+
