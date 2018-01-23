bb8549bd0d15cc93bef8aa656650f2d64f706c8d
Debal Chakrabarty <debalchakrabarty@gmail.com>
2018-01-23 13:52:48 +0530

    Create BikeShare.py

---
+++ udacity/BikeShare.py
@@ -0,0 +1,235 @@
+
+# coding: utf-8
+
+# # 2016 US Bike Share Activity Snapshot
+# 
+# ## Table of Contents
+# - [Introduction](#intro)
+# - [Posing Questions](#pose_questions)
+# - [Data Collection and Wrangling](#wrangling)
+#   - [Condensing the Trip Data](#condensing)
+# - [Exploratory Data Analysis](#eda)
+#   - [Statistics](#statistics)
+#   - [Visualizations](#visualizations)
+# - [Performing Your Own Analysis](#eda_continued)
+# - [Conclusions](#conclusions)
+# 
+# <a id='intro'></a>
+# ## Introduction
+# 
+# > **Tip**: Quoted sections like this will provide helpful instructions on how to navigate and use a Jupyter notebook.
+# 
+# Over the past decade, bicycle-sharing systems have been growing in number and popularity in cities across the world. Bicycle-sharing systems allow users to rent bicycles for short trips, typically 30 minutes or less. Thanks to the rise in information technologies, it is easy for a user of the system to access a dock within the system to unlock or return bicycles. These technologies also provide a wealth of data that can be used to explore how these bike-sharing systems are used.
+# 
+# In this project, you will perform an exploratory analysis on data provided by [Motivate](https://www.motivateco.com/), a bike-share system provider for many major cities in the United States. You will compare the system usage between three large cities: New York City, Chicago, and Washington, DC. You will also see if there are any differences within each system for those users that are registered, regular users and those users that are short-term, casual users.
+
+# <a id='pose_questions'></a>
+# ## Posing Questions
+# 
+# Before looking at the bike sharing data, you should start by asking questions you might want to understand about the bike share data. Consider, for example, if you were working for Motivate. What kinds of information would you want to know about in order to make smarter business decisions? If you were a user of the bike-share service, what factors might influence how you would want to use the service?
+# 
+# **Question 1**: Cities that use bikes the most?
+# 
+# **Question 2**: Cities where people use the bikes for more time than other areas?
+# 
+# > **Tip**: If you double click on this cell, you will see the text change so that all of the formatting is removed. This allows you to edit this block of text. This block of text is written using [Markdown](http://daringfireball.net/projects/markdown/syntax), which is a way to format text using headers, links, italics, and many other options using a plain-text syntax. You will also use Markdown later in the Nanodegree program. Use **Shift** + **Enter** or **Shift** + **Return** to run the cell and show its rendered form.
+
+# <a id='wrangling'></a>
+# ## Data Collection and Wrangling
+# 
+# Now it's time to collect and explore our data. In this project, we will focus on the record of individual trips taken in 2016 from our selected cities: New York City, Chicago, and Washington, DC. Each of these cities has a page where we can freely download the trip data.:
+# 
+# - New York City (Citi Bike): [Link](https://www.citibikenyc.com/system-data)
+# - Chicago (Divvy): [Link](https://www.divvybikes.com/system-data)
+# - Washington, DC (Capital Bikeshare): [Link](https://www.capitalbikeshare.com/system-data)
+# 
+# If you visit these pages, you will notice that each city has a different way of delivering its data. Chicago updates with new data twice a year, Washington DC is quarterly, and New York City is monthly. **However, you do not need to download the data yourself.** The data has already been collected for you in the `/data/` folder of the project files. While the original data for 2016 is spread among multiple files for each city, the files in the `/data/` folder collect all of the trip data for the year into one file per city. Some data wrangling of inconsistencies in timestamp format within each city has already been performed for you. In addition, a random 2% sample of the original data is taken to make the exploration more manageable. 
+# 
+# **Question 2**: However, there is still a lot of data for us to investigate, so it's a good idea to start off by looking at one entry from each of the cities we're going to analyze. Run the first code cell below to load some packages and functions that you'll be using in your analysis. Then, complete the second code cell to print out the first trip recorded from each of the cities (the second line of each data file).
+# 
+# > **Tip**: You can run a code cell like you formatted Markdown cells above by clicking on the cell and using the keyboard shortcut **Shift** + **Enter** or **Shift** + **Return**. Alternatively, a code cell can be executed using the **Play** button in the toolbar after selecting it. While the cell is running, you will see an asterisk in the message to the left of the cell, i.e. `In [*]:`. The asterisk will change into a number to show that execution has completed, e.g. `In [1]`. If there is output, it will show up as `Out [1]:`, with an appropriate number to match the "In" number.
+
+# In[2]:
+
+
+## import all necessary packages and functions.
+import csv # read and write csv files
+from datetime import datetime # operations to parse dates
+from pprint import pprint # use to print data structures like dictionaries in
+import calendar                          # a nicer way than the base print function.
+
+
+# In[5]:
+
+
+def print_first_point(filename):
+    """
+    This function prints and returns the first data point (second row) from
+    a csv file that includes a header row.
+    """
+    # print city name for reference
+    city = filename.split('-')[0].split('/')[-1]
+    print('\nCity: {}'.format(city))
+    
+    with open(filename, 'r') as f_in:
+        ## TODO: Use the csv library to set up a DictReader object. ##
+        ## see https://docs.python.org/3/library/csv.html           ##
+        trip_reader =csv.DictReader(f_in)
+        
+        ## TODO: Use a function on the DictReader object to read the     ##
+        ## first trip from the data file and store it in a variable.     ##
+        ## see https://docs.python.org/3/library/csv.html#reader-objects ##
+        first_trip =next(trip_reader)
+        
+        ## TODO: Use the pprint library to print the first trip. ##
+        ## see https://docs.python.org/3/library/pprint.html     ##
+        pprint(first_trip)
+    # output city name and first trip for later testing
+    return (city, first_trip)
+
+# list of files for each city
+data_files = ['NYC-CitiBike-2016.csv',
+              'Chicago-Divvy-2016.csv',
+              'Washington-CapitalBikeshare-2016.csv',]
+
+# print the first trip from each file, store in dictionary
+example_trips = {}
+for data_file in data_files:
+    city, first_trip = print_first_point(data_file)
+    example_trips[city] = first_trip
+
+
+# If everything has been filled out correctly, you should see below the printout of each city name (which has been parsed from the data file name) that the first trip has been parsed in the form of a dictionary. When you set up a `DictReader` object, the first row of the data file is normally interpreted as column names. Every other row in the data file will use those column names as keys, as a dictionary is generated for each row.
+# 
+# This will be useful since we can refer to quantities by an easily-understandable label instead of just a numeric index. For example, if we have a trip stored in the variable `row`, then we would rather get the trip duration from `row['duration']` instead of `row[0]`.
+# 
+# <a id='condensing'></a>
+# ### Condensing the Trip Data
+# 
+# It should also be observable from the above printout that each city provides different information. Even where the information is the same, the column names and formats are sometimes different. To make things as simple as possible when we get to the actual exploration, we should trim and clean the data. Cleaning the data makes sure that the data formats across the cities are consistent, while trimming focuses only on the parts of the data we are most interested in to make the exploration easier to work with.
+# 
+# You will generate new data files with five values of interest for each trip: trip duration, starting month, starting hour, day of the week, and user type. Each of these may require additional wrangling depending on the city:
+# 
+# - **Duration**: This has been given to us in seconds (New York, Chicago) or milliseconds (Washington). A more natural unit of analysis will be if all the trip durations are given in terms of minutes.
+# - **Month**, **Hour**, **Day of Week**: Ridership volume is likely to change based on the season, time of day, and whether it is a weekday or weekend. Use the start time of the trip to obtain these values. The New York City data includes the seconds in their timestamps, while Washington and Chicago do not. The [`datetime`](https://docs.python.org/3/library/datetime.html) package will be very useful here to make the needed conversions.
+# - **User Type**: It is possible that users who are subscribed to a bike-share system will have different patterns of use compared to users who only have temporary passes. Washington divides its users into two types: 'Registered' for users with annual, monthly, and other longer-term subscriptions, and 'Casual', for users with 24-hour, 3-day, and other short-term passes. The New York and Chicago data uses 'Subscriber' and 'Customer' for these groups, respectively. For consistency, you will convert the Washington labels to match the other two.
+# 
+# 
+# **Question 3a**: Complete the helper functions in the code cells below to address each of the cleaning tasks described above.
+
+# In[6]:
+
+
+def duration_in_mins(datum, city):
+    """
+    Takes as input a dictionary containing info about a single trip (datum) and
+    its origin city (city) and returns the trip duration in units of minutes.
+    
+    Remember that Washington is in terms of milliseconds while Chicago and NYC
+    are in terms of seconds. 
+    
+    HINT: The csv module reads in all of the data as strings, including numeric
+    values. You will need a function to convert the strings into an appropriate
+    numeric type when making your transformations.
+    see https://docs.python.org/3/library/functions.html
+    """
+    # YOUR CODE HERE
+    if city != 'Washington':
+        duration = int(datum['tripduration'])/60
+    else:
+        duration = (int(datum['Duration (ms)']))/((10**3)*(60))
+    return duration
+
+
+# Some tests to check that your code works. There should be no output if all of
+# the assertions pass. The `example_trips` dictionary was obtained from when
+# you printed the first trip from each of the original data files.
+tests = {'NYC': 13.9833,
+         'Chicago': 15.4333,
+         'Washington': 7.1231}
+
+for city in tests:
+    assert abs(duration_in_mins(example_trips[city], city) - tests[city]) < .001
+
+
+# In[9]:
+
+
+def time_of_trip(datum, city):
+    """
+    Takes as input a dictionary containing info about a single trip (datum) and
+    its origin city (city) and returns the month, hour, and day of the week in
+    which the trip was made.
+    
+    Remember that NYC includes seconds, while Washington and Chicago do not.
+    
+    HINT: You should use the datetime module to parse the original date
+    strings into a format that is useful for extracting the desired information.
+    see https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+    """
+    days=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+    # YOUR CODE HERE
+    if city == 'NYC':
+        d1 = datetime.strptime(datum['starttime'], '%m/%d/%Y %H:%I:%M')
+        #day_of_week = calendar.day_name(d1.weekday())
+        month = d1.day
+        hour= datetime.strptime(datum['stoptime'].split(' ')[-1],"%H:%I:%M")-datetime.strptime(datum['starttime'].split(' ')[-1],"%H:%I:%M")
+    elif city == 'Chicago':
+        d1 = datetime.strptime(datum['starttime'], '%m/%d/%Y %I:%M')
+        month = d1.day
+        hour= datetime.strptime(datum['stoptime'].split(' ')[-1],"%I:%M")-datetime.strptime(datum['starttime'].split(' ')[-1],"%I:%M")
+
+    else:
+        d1 = datetime.strptime(datum['Start date'], '%m/%d/%Y %I:%M')
+        month = d1.day
+        hour= datetime.strptime(datum['End date'].split(' ')[-1],"%I:%M")-datetime.strptime(datum['Start Date'].split(' ')[-1],"%I:%M")
+    return (month, hour, day_of_week)
+
+
+# Some tests to check that your code works. There should be no output if all of
+# the assertions pass. The `example_trips` dictionary was obtained from when
+# you printed the first trip from each of the original data files.
+tests = {'NYC': (1, 0, 'Friday'),
+         'Chicago': (3, 23, 'Thursday'),
+         'Washington': (3, 22, 'Thursday')}
+
+for city in tests:
+    assert time_of_trip(example_trips[city], city) == tests[city]
+
+
+# In[ ]:
+
+
+def type_of_user(datum, city):
+    """
+    Takes as input a dictionary containing info about a single trip (datum) and
+    its origin city (city) and returns the type of system user that made the
+    trip.
+    
+    Remember that Washington has different category names compared to Chicago
+    and NYC. 
+    """
+    
+    # YOUR CODE HERE
+    
+    return user_type
+
+
+# Some tests to check that your code works. There should be no output if all of
+# the assertions pass. The `example_trips` dictionary was obtained from when
+# you printed the first trip from each of the original data files.
+tests = {'NYC': 'Customer',
+         'Chicago': 'Subscriber',
+         'Washington': 'Subscriber'}
+
+for city in tests:
+    assert type_of_user(example_trips[city], city) == tests[city]
+
+
+# **Question 3b**: Now, use the helper functions you wrote above to create a condensed data file for each city consisting only of the data fields indicated above. In the `/examples/` folder, you will see an example datafile from the [Bay Area Bike Share](http://www.bayareabikeshare.com/open-data) before and after conversion. Make sure that your output is formatted to be consistent with the example file.
+
+# In[ ]:
+
+
+
+
